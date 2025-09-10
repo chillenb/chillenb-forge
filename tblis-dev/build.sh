1fc7c6d297c0ed544d964c12fce178aba5777cdd
chillenb <chillenbrand15@gmail.com>
2025-09-09 20:16:43 -0400

    add tblis-dev

---
+++ tblis-dev/build.sh
@@ -0,0 +1,17 @@
+CFLAGS=$(echo "${CFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
+CFLAGS=$(echo "${CFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
+CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
+CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
+unset PYTHON
+
+cmake -B build -S . \
+      ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DENABLE_HWLOC=ON \
+      -DBLIS_THREAD_MODEL=openmp \
+      -DENABLE_THREAD_MODEL=openmp \
+      -DBLIS_CONFIG_FAMILY=x86_64 \
+      -DENABLE_STATIC=OFF
+cmake --build build -j${CPU_COUNT}
+cmake --install build
+
