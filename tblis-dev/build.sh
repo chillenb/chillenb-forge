dfa85a26204bc1ef478beb483eb8e03aa95da876
chillenb <chillenbrand15@gmail.com>
2025-11-25 16:35:47 -0500

    update tblis and add macos

--- tblis-dev/build.sh
+++ tblis-dev/build.sh
@@ -4,13 +4,15 @@ CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-march=[a-zA-Z0-9]*//g")
 CXXFLAGS=$(echo "${CXXFLAGS}" | sed "s/-mtune=[a-zA-Z0-9]*//g")
 unset PYTHON
 
+echo "Building for TBLIS_ARCH=${TBLIS_ARCH}"
+
 cmake -B build -S . \
       ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DENABLE_HWLOC=ON \
       -DBLIS_THREAD_MODEL=openmp \
       -DENABLE_THREAD_MODEL=openmp \
-      -DBLIS_CONFIG_FAMILY=x86_64 \
+      -DBLIS_CONFIG_FAMILY=${TBLIS_ARCH} \
       -DENABLE_STATIC=OFF
 cmake --build build -j${CPU_COUNT}
 cmake --install build
